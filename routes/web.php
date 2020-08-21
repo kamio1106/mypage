1b1e33a93448595efc20fca1f7c025d216276481
大塚凜太郎 <rintaro_otsuka@spice-factory.co.jp>
2020-08-22 01:03:18 +0900

    basic追加

--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/mypage/introduction', function () {
     return view('mypage/introduction');
 });
-Route::get('/mypage/diary','DiaryController@index');
-Route::get('/mypage/diary/create','DiaryController@create')->name('diary.create');
-Route::post('/mypage/diary/upload', 'DiaryController@upload');
-Route::get('/mypage/diary/{id}/edit', 'DiaryController@edit')->name('diary.edit');
-Route::post('/mypage/diary/{id}/updata', 'DiaryController@updata')->name('diary.updata');
+//Route::get('/mypage/diary','DiaryController@index');
+//Route::get('/mypage/diary/create','DiaryController@create')->name('diary.create');
+//Route::post('/mypage/diary/upload', 'DiaryController@upload');
+//Route::get('/mypage/diary/{id}/edit', 'DiaryController@edit')->name('diary.edit');
+//Route::post('/mypage/diary/{id}/updata', 'DiaryController@updata')->name('diary.updata');
 
+Route::group(['middleware' => 'auth.very_basic'], function() {
+
+    Route::get('/mypage/information', function () {
+        return view('mypage/information');
+    });
+
+});
 
 
 Route::get('/mypage/collection', function () {
     return view('mypage/collection');
 });
 
-Route::get('/mypage/information', function () {
-    return view('mypage/information');
-});
 Route::get('/mypage/collection/board', function () {
     return view('mypage/collection/board');
 });
